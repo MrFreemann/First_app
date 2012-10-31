c3ece387d56dde6eb36cc18233f372f7a586222a
unknown <dave@.(none)>
2012-10-31 22:24:30 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Impressbox::Application.config.secret_token = 'e14689f5aac7619200edac6eeba9bfde46f094dd10c17733af7651d5a8e841f513836bb7ec41df3604a91fda4ce33114644676eae63e91b218a5109ddbfeb5f9'

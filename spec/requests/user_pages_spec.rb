3b9c5a0ad5ec903d8e10a0b057250a75fd58ae75
David <ggs.david@gmail.com>
2013-02-05 09:52:24 +0200

    basic signing in done

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,10 +13,58 @@ describe "User Pages" do
   end
   
   describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    
     before { visit user_path(user) }
     
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-  
+
+  describe "signup" do
+
+    before { visit signup_path }
+    
+    let(:submit) { "Create my account" }
+
+    describe "with invalid info" do
+      it "should not create a user" do
+
+        old_count = User.count 
+        click_button "Create my account"
+        new_count = User.count
+        new_count.should == old_count
+      end
+
+      describe "after submition" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up' ) }
+        it { should have_content('error') }
+        it { should_not have_content('Password digest') }
+      end
+    end
+
+    describe "with valid information" do
+
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving a user" do
+        before { click_button submit }
+
+        let(:user) { User.find_by_email("user@example.com") }
+        
+        it { should have_selector('title', text: user.name)}
+        it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+      end
+    end
+  end
 end 
